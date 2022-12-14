01bfb10dd08b7a50e9e1c60f208555324645317c
Kyle Mulleady <kyle@windwardapps.com>
2022-12-13 16:46:25 -1000

    UserActivityList fixes

--- src/ui/resources/userActivity.js
+++ src/ui/resources/userActivity.js
@@ -1,4 +1,13 @@
-import { Datagrid, DateField, FilterList, FilterListItem, FunctionField, List, TextField } from 'react-admin'
+import {
+  Datagrid,
+  DateField,
+  FilterList,
+  FilterListItem,
+  FunctionField,
+  List,
+  Pagination as RAPagination,
+  TextField
+} from 'react-admin'
 import { Card, CardContent } from '@mui/material'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
@@ -8,7 +17,7 @@ export const UserActivityList = () => {
   // const filters = [<SearchInput source="userId" />, <SearchInput source="event" />, <SearchInput source="data" />]
 
   return (
-    <List aside={<FilterSidebar />}>
+    <List aside={<FilterSidebar />} pagination={<Pagination />}>
       <Datagrid bulkActionButtons={false}>
         <TextField source="id" />
         <TextField source="userId" />
@@ -31,19 +40,45 @@ function renderData(record) {
   let lines = []
   const keys = Object.keys(data).sort()
   for (const k of keys) {
-    const v = data[k]
-    lines.push(
-      <tr key={k}>
-        <td style={{ color: '#778', verticalAlign: 'top' }}>{k}: </td>
-        <td style={{ verticalAlign: 'top' }}>{v}</td>
-      </tr>
-    )
+    let v = data[k]
+    if (typeof v === 'object') {
+      let items = []
+      for (const [k2, v2] of Object.entries(v)) {
+        items.push(
+          <tr key={k2}>
+            <td style={{ color: '#778', verticalAlign: 'top' }}>
+              <div style={{ paddingLeft: 20 }}>{k2}: </div>
+            </td>
+            <td style={{ verticalAlign: 'top' }}>{v2}</td>
+          </tr>
+        )
+      }
+      lines.push(
+        <>
+          <tr key={k}>
+            <td style={{ color: '#778', verticalAlign: 'top' }}>{k}: </td>
+          </tr>
+          {items}
+        </>
+      )
+    } else {
+      lines.push(
+        <tr key={k}>
+          <td style={{ color: '#778', verticalAlign: 'top' }}>{k}: </td>
+          <td style={{ verticalAlign: 'top' }}>{v}</td>
+        </tr>
+      )
+    }
   }
   return (
     <table>
       <tbody>{lines}</tbody>
     </table>
   )
+}
+
+function Pagination() {
+  return <RAPagination perPage={100} rowsPerPageOptions={[10, 25, 50, 100]} />
 }
 
 let _events
@@ -68,7 +103,7 @@ function FilterSidebar() {
   }, [])
 
   return (
-    <Card sx={{ order: -1, mr: 2, mt: '64px', width: 300 }}>
+    <Card sx={{ order: -1, mr: 2, mt: '64px', mb: '52px', width: 380 }}>
       <CardContent>
         {/* <SavedQueriesList /> */}
         {/* <FilterLiveSearch /> */}
