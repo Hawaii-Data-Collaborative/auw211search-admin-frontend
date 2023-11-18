3e01759c3add381564a7a826f9590be957765939
Kyle Mulleady <kmulleady@hawaiidata.org>
2023-11-17 15:22:17 -1000

    production config

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 echo "[deploy] scp'ing ..."
-scp build.tar.gz wwa:/var/www/auwsearch.windwardapps.com/auw-admin-ui/
+scp build.tar.gz auw1:/var/www/searchengine-admin-frontend/
 
 echo "[deploy] ssh'ing ..."
-ssh wwa bash << EOF
-cd /var/www/auwsearch.windwardapps.com/auw-admin-ui
+ssh auw1 bash << EOF
+cd /var/www/searchengine-admin-frontend
 if [ -d "./build" ]; then
   echo "[deploy] moving existing build dir to build-old ..."
   rm -rf build-old
